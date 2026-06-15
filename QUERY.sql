e47bd4ace275ad5c09f6090155e0a83193671802
Fahim-rafi24 <fahimrafibd2003@gmail.com>
2026-06-15 14:52:10 +0600

    update query.sql 4 & 5 query

--- QUERY.sql
+++ QUERY.sql
@@ -15,3 +15,14 @@ WHERE full_name LIKE 'Tanvir%' OR full_name LIKE '%Haque%';
 SELECT booking_id, user_id, match_id, COALESCE(payment_status, 'Action Required') AS systematic_status
 FROM Bookings
 WHERE payment_status IS NULL;
+
+-- Query 4: Retrieve match booking details along with the User's full name and the scheduled Match fixture teams.
+SELECT b.booking_id, u.full_name, m.fixture, b.total_cost
+FROM Bookings b
+INNER JOIN Users u ON b.user_id = u.user_id
+INNER JOIN Matches m ON b.match_id = m.match_id;
+
+-- Query 5: Display a comprehensive list of all users and their booking IDs, ensuring that fans who have never bought a ticket are still listed.
+SELECT u.user_id, u.full_name, b.booking_id
+FROM Users u
+LEFT JOIN Bookings b ON u.user_id = b.user_id;
